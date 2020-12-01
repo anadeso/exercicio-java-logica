2a9ca2b3854dd83f7e98226592c3dc2067f59f59
anadeso <anadeso90@gmail.com>
2020-11-30 21:10:37 -0300

    Matrizes

---
+++ src/com/company/aula20/Matrizes.java
@@ -0,0 +1,20 @@
+package com.company.aula20;
+
+public class Matrizes {
+
+    public static void main(String[] args) {
+
+        double[][] notasAlunos = new double[30][4];
+
+        notasAlunos[0][0] = 8;
+        notasAlunos[0][1] = 9;
+        notasAlunos[0][2] = 10;
+        notasAlunos[0][3] = 10;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 10;
+        notasAlunos[1][2] = 8;
+        notasAlunos[1][3] = 7;
+
+    }
+}
